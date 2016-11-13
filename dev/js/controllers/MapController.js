b8d2a2185d9f3f1533f81c2ab00813505fc34ca1
pogithub <poamrong@rith.com>
2016-11-12 18:47:31 -0800

    Add account for removed markers in mapping

--- dev/js/controllers/MapController.js
+++ dev/js/controllers/MapController.js
@@ -20,18 +20,7 @@ app.controller("MapController", ['$scope', '$http', "$interval", 'myService', fu
     markers: {}
   });
 
-  myService.sayHello("Bird is the word..!");
-
-  var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+  // myService.sayHello("Bird is the word..!");
 
   // Fetch .JSON data
   var fetchData = function () {
@@ -79,12 +68,13 @@ app.controller("MapController", ['$scope', '$http', "$interval", 'myService', fu
 
   // Create and place markers
   var doCreateMarkers = function () {
+    $scope.markers = {};
+
     $scope.data.filter(function(data) {
       $scope.markers[data.shortname] = {
         title: data.name,
         lat: data.coords[0],
         lng: data.coords[1],
-        icon: greenIcon,
         message: data.name +"<br><span style='font-size: 12px; font-weight: 600;'>" + data.employees + " Employees</span>",
       };
     });
